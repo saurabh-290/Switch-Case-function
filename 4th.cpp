0f649c110206b0f465bfd47394ddc076a25177ff
saurabh-290 <132060392+saurabh-290@users.noreply.github.com>
2024-01-07 23:11:27 +0530

    Add files via upload

---
+++ 4th.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+using namespace std;
+
+
+void multiplise(int num1, int num2) {
+  int result = num1 * num2;
+  cout << "The result is: " << result << endl;
+}
+
+int main() {
+
+  int num1, num2;
+  cout << "Enter The No.1:" << endl;
+  cin >> num1;
+  cout << "Enter The No.2:" << endl;
+  cin >> num2;
+
+  multiplise(num1, num2);
+
+
+ return 0;
+}
+
+
+ // First Comment all Upper Code Includeing from line 0 To 23 then commentout below code for run.
+
+
+
+// #include <iostream>
+// using namespace std;
+
+
+// int multiplise(int num1, int num2) {
+//   int result = num1 * num2;
+//   return result;
+// }
+
+// int main() {
+
+//   int num1, num2;
+//   cout << "Enter The No.1:" << endl;
+//   cin >> num1;
+//   cout << "Enter The No.2:" << endl;
+//   cin >> num2;
+
+//   int result = multiplise(num1, num2);
+
+//   cout << "The final result is:" << result << endl;
+
+//    return 0;
+//   }
